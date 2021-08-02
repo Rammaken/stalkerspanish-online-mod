fa6dcc6ea3342cf7c1fbb54247f60bc0ba00266f
Rammaken <laggamer19573@gmail.com>
2021-08-02 15:54:00 -0400

    Mod porteado a OpenMultiPlayer 1.2

---
+++ _dedicated_/dedicated_fmp_omp_test.bat
@@ -0,0 +1,2 @@
+cd ..\bin
+..\_dedicated_\xrEngine.exe -dedicated -i -nosound  -fsltx ..\fsgame_dedicated.ltx -start server(omp_test/fmp/timelimit=60) client(localhost)
---
+++ _dedicated_/dedicated_rp_omp_test.bat
@@ -0,0 +1,2 @@
+cd ..\bin
+..\_dedicated_\xrEngine.exe -dedicated -i  -fsltx ..\fsgame_dedicated.ltx -start server(omp_test/rp/timelimit=60) client(localhost)
